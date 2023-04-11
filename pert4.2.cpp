f78d5c6d9583f637ce7d54257180b92134d812ed
mikosta <128479246+mikosta@users.noreply.github.com>
2023-04-11 13:56:15 +0700

    Add files via upload

---
+++ pert4.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float m, cm, inci;
+    
+    system("cls");
+    
+    cout << "masukan ukuran meter" << endl;
+    cin >> m;
+    
+    cm = m * 100;
+    inci = m * 100 / 2.54;
+    cout << "ukuran dalam cm " << cm << endl;
+    cout << "ukuran dalam inci " << inci << endl;
+    
+    return 0;
+}
+
---
+++ pertemuan4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    
+    cout << "masukkan nilai : ";
+    cin >> i;
+    
+    cout << "nilai yang digunakan adalah " << i << endl;
+    cout << "nilai jika digandakan " << i*2 << endl;
+    
+   
+    return 0;
+}
+
